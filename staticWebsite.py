ba6606dadf108545176ea00d783261c19de19206
F1neg4n <126989239+F1neg4n@users.noreply.github.com>
2023-03-13 16:53:30 -0300

    Create staticWebsite.py

---
+++ staticWebsite.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# Script by F1neg4n
+
+import os
+
+
+def welcome():
+    welc = 'Static Website'
+    info = '[INFO] Python script to create base for static website'
+    os.system('clear')
+    print(welc + '\n' + '*' * len(welc))
+    print(info)
+    print('------------')
+    return
+
+def projectName():
+    welcome()
+    try: 
+        projectName = input('[+] Enter the name project: ')
+        while(projectName == ''):
+            projectName = input('[+] Please, enter the name project: ')
+    except(KeyboardInterrupt):
+        os.system('clear')
+        welcome()
+        print('[-] Interrupted by user!')
+        exit()
+    return projectName
+
+def createProject():
+    name = projectName()
+    os.system('clear')
+    welcome()
+    print('[+] Creating new project "'+name+'"...')
+    os.system('mkdir ' + name)
+    os.chdir(name + '/')
+    os.system('mkdir static')
+    os.system('touch index.html')
+    os.chdir('static/')
+    os.system('mkdir css js img')
+    os.chdir('css/')
+    os.system('touch style.css')
+    os.chdir('../')
+    os.chdir('js/')
+    os.system('touch index.js')
+    print('[+] Done!')
+    return
+
+if __name__ == '__main__':
+    createProject()
